e65037f47327563f87eeb7e84209f431e851f010
MKSisti <mohammedkarimsisti@gmail.com>
2022-02-22 18:35:23 +0100

    add change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/', (req, res) => {
   res.json({
       "name":"the best api you ever witnessed",
-      "data": [1,2,3,4,5,6,7,8]
+      "data": [1,2,3,4,5,6]
   })
 })
 
